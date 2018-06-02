0396c4635abff11b906870525fe1b4c47894ffdb
swordfall <2506597416@qq.com>
2018-06-02 15:38:05 +0800

    二分查找

---
+++ README.md
@@ -0,0 +1,2 @@
+# interview_bigdata
+关于大数据的面试题，包括hadoop、hbase、hive、spark、storm、zookeeper、kafka、flume、logstash、redis、ELK、算法等等
---
+++ src/main/java/cn/itcast/common/Out.java
@@ -0,0 +1,16 @@
+package cn.itcast.common;
+
+public class Out {
+
+    static void write(int[] a, String name){
+
+    }
+
+    static void write(double[] a, String name){
+
+    }
+
+    static void write(String[] a, String name){
+
+    }
+}
